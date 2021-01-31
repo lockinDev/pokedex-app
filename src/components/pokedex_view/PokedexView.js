754025cbb437965f7d4b2439d25ba28b210cf8eb
lockinDev <javieralmario12@gmail.com>
2021-01-30 20:36:31 -0400

    States are created in PokedexView component for loader management and internationalization.

--- src/components/pokedex_view/PokedexView.js
+++ src/components/pokedex_view/PokedexView.js
@@ -1,10 +1,26 @@
 import React, { Fragment } from "react";
-import lngs from '../../helpers/languages'
+import lngs from "../../helpers/languages";
 import { Navigation } from "../navigation/Navigation";
 
-import './pokedex.css';
+import "./pokedex.css";
 
 export const PokedexView = () => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const [language, setLanguage] = useState();
+
+  const { i18n } = useTranslation();
+
+  const handleChangeLanguage = (language) => {
+    console.log(language);
+    localStorage.setItem("language", language);
+    setLanguage(language);
+    i18n.changeLanguage(language);
+  };
+
+  if (isLoading) {
+    return <Loader />;
+  }
 
   return (
     <Fragment>
@@ -16,12 +32,10 @@ export const PokedexView = () => {
             alt={lngs[lng].alt}
             onClick={() => handleChangeLanguage(lng)}
           ></img>
-        ))}        
+        ))}
       </div>
 
       <Navigation />
-
-
     </Fragment>
   );
 };
