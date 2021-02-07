90f632cb3e59d9642a7e5e94c2ea4e313a329682
lockinDev <javieralmario12@gmail.com>
2021-02-06 20:26:18 -0400

    Added internationalization to the component card.

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,7 +1,9 @@
 import React from "react";
-import './card.css'
+import "./card.css";
 
 export const Card = ({ pokemon: { name, id, sprites, types }, onClick }) => {
+  const [typesFetch, setTypesFetch] = useState([]);
+
   // get pokemon image
   const imgURL = sprites.other["official-artwork"].front_default;
 
@@ -11,15 +13,28 @@ export const Card = ({ pokemon: { name, id, sprites, types }, onClick }) => {
   // pad pokemon id with zeros
   const paddedId = "#" + id.toString().padStart(3, "000");
 
+  useEffect(() => {
+    fetchTypes(types, typesFetch, setTypesFetch);
+    console.log();
+  }, [types]);
+
   return (
     <div className={`card ${cssClass}`} onClick={onClick}>
       <div className="bg-pokeball"></div>
       <span className="pokemon-id">{paddedId}</span>
 
       <div className="card-title">
-        <h2>{name.replace(/-/g, " ")}</h2>  
+        <h2>{name.replace(/-/g, " ")}</h2>
 
-        <div className="pokemon-types"></div>
+        <div className="pokemon-types">
+          {typesFetch.map((t, i) => {
+            return (
+              <span className="type" key={i}>
+                {t}
+              </span>
+            );
+          })}
+        </div>
 
         <div className="pokemon-image">
           <img width={100} alt={name} src={imgURL} />
