72aecd5bcd1621fcd401f721e97094816273f67c
lockinDev <javieralmario12@gmail.com>
2021-02-16 18:55:24 -0400

    Added styles to the Overlay component and added to the DetailsView component.

--- src/components/details_view/DetailsView.js
+++ src/components/details_view/DetailsView.js
@@ -1,44 +1,39 @@
-import React, { Fragment, useRef, useState } from 'react'
+import React, { Fragment, useRef, useState } from "react";
 import ReactDOM from "react-dom";
 
 import { Card } from "../card/Card";
 import { BackButton } from "../back_button/BackButton";
+import { Overlay } from "../overlay/Overlay";
 
-import './details_view.css';
-
-
-
+import "./details_view.css";
 
 export const DetailsView = (props) => {
+  const ref = useRef(null);
+  const [pokemon, setPokemon] = useState(props.pokemon);
+  const [hideOverlay, setHideOverlay] = useState(false);
 
-    const ref = useRef(null);
-    const [pokemon, setPokemon] = useState(props.pokemon);
-    const [hideOverlay, setHideOverlay] = useState(false);
+  const handleBackClick = () => {
+    setHideOverlay(true);
 
-    const handleBackClick = () => {
-        setHideOverlay(true);
-    
-        ref.current.classList.add("hidden");
-    
-        setTimeout(() => {
-          props.setSelectedPokemon(-1);
-        }, 500);
-      };
-    
-     const setPokemonData = (data) => {
-        setPokemon(data);
-      }
-    
+    ref.current.classList.add("hidden");
 
+    setTimeout(() => {
+      props.setSelectedPokemon(-1);
+    }, 500);
+  };
 
-      return ReactDOM.createPortal(
-        <Fragment >
-          <div className="details-view-container shown" ref={ref}>
-            <BackButton onClick={handleBackClick} />
-            <Card pokemon={pokemon} />
-           
-          </div>
-        </Fragment >,
-        document.body
-      );
-    };
+  const setPokemonData = (data) => {
+    setPokemon(data);
+  };
+
+  return ReactDOM.createPortal(
+    <Fragment>
+      <Overlay hidden={hideOverlay} onClick={handleBackClick} />
+      <div className="details-view-container shown" ref={ref}>
+        <BackButton onClick={handleBackClick} />
+        <Card pokemon={pokemon} />
+      </div>
+    </Fragment>,
+    document.body
+  );
+};
