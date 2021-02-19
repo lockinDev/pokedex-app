088edd0aa2845c63e0828ec4bf44ae78f495679a
lockinDev <javieralmario12@gmail.com>
2021-02-19 17:44:43 -0400

    Added tabs and styles of the Details component.

--- src/components/details/Details.js
+++ src/components/details/Details.js
@@ -1,12 +1,15 @@
-import React, { useState } from 'react';
-import { useTranslation } from 'react-i18next';
+import React, { useState } from "react";
+import { useTranslation } from "react-i18next";
 
-import './details.css'
-
+import "./details.css";
 
 export const Details = ({ pokemon, setPokemonData }) => {
-
   const { t } = useTranslation();
+
+  const switchTab = (e) => {
+    e.preventDefault();
+    setTab(e.target.dataset.tab);
+  };
 
   const getClassName = (tabName) => {
     return `tab-switch ${tab === tabName ? "active" : ""}`;
@@ -14,9 +17,11 @@ export const Details = ({ pokemon, setPokemonData }) => {
 
   return (
     <div className="details">
-
-
-
+      <div className="tabs-switch-container">
+        <button>{t("details.about.title")}</button>
+        <button>{t("details.stats.title")}</button>
+        <button>{t("details.evol.title")}</button>
+      </div>
     </div>
-)
+  );
 };
