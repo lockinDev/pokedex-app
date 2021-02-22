97c4e4e802bfca0c093a10b786815cc920002771
lockinDev <javieralmario12@gmail.com>
2021-02-22 15:39:32 -0400

    Added tab element to the Details component, with its respective styles.

--- src/components/details/Details.js
+++ src/components/details/Details.js
@@ -4,27 +4,60 @@ import { useTranslation } from "react-i18next";
 import "./details.css";
 
 export const Details = ({ pokemon, setPokemonData }) => {
+  const [tab, setTab] = useState("about");
+
   const { t } = useTranslation();
+
+  const sprites = pokemon.sprites.other;
+  const imgURL = sprites["official-artwork"].front_default;
 
   const switchTab = (e) => {
     e.preventDefault();
-    setTab(e.target.dataset.tab); 
+    setTab(e.target.dataset.tab);
   };
 
   const getClassName = (tabName) => {
     return `tab-switch ${tab === tabName ? "active" : ""}`;
   };
 
+  const changePokemon = (pokemon) => {
+    setTab("about");
+    setPokemonData(pokemon);
+  };
+
   return (
     <div className="details">
-
-      <img src={ imgURL } className="pokemon-image" alt={ pokemon.name } />
+      <img src={imgURL} className="pokemon-image" alt={pokemon.name} />
 
       <div className="tabs-switch-container">
-        <button>{t("details.about.title")}</button>
-        <button>{t("details.stats.title")}</button>
-        <button>{t("details.evol.title")}</button>
+        <button
+          className={getClassName("about")}
+          data-tab="about"
+          onClick={switchTab}
+        >
+          {t("details.about.title")}
+        </button>
+        <button
+          className={getClassName("base-stats")}
+          data-tab="base-stats"
+          onClick={switchTab}
+        >
+          {t("details.stats.title")}
+        </button>
+        <button
+          className={getClassName("evolution")}
+          data-tab="evolution"
+          onClick={switchTab}
+        >
+          {t("details.evol.title")}
+        </button>
       </div>
+
+      {tab === "about" }
+
+      {tab === "base-stats" }
+
+      {tab === "evolution" }
     </div>
   );
 };
