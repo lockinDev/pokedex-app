a2f3d0d6a856882246e4a07b497c334af447f406
lockinDev <javieralmario12@gmail.com>
2021-03-01 14:43:26 -0400

    Added functions to the Evolution component, where the sprite and the pokemon's own id are extracted from the api.

--- src/components/details/Tabs/Evolution.js
+++ src/components/details/Tabs/Evolution.js
@@ -1,99 +1,113 @@
-import React from 'react'
-import { useTranslation } from 'react-i18next';
+import React from "react";
+import { useTranslation } from "react-i18next";
 
-import { Loader } from '../../Loader';
-import { fetchPokemonEvolutionChain, fetchPokemonData } from '../../../api';
-
-
-
+import { Loader } from "../../Loader";
+import { fetchPokemonEvolutionChain, fetchPokemonData } from "../../../api";
 
 export const Evolution = ({ pokemon, changePokemon }) => {
+  const [currentEvolution, setCurrentEvolution] = useState([]);
+  const [evolutionChain, setEvolutionChain] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const { t } = useTranslation();
+  const triggersDisplayName = {
+    "level-up": "Lvl",
+    trade: "Trade",
+    "use-item": "Use",
+  };
+
+  useEffect(() => {
+    setLoading(true);
+
+    fetchPokemonEvolutionChain(pokemon.id).then((data) => {
+      setEvolutionChain([]);
+      setCurrentEvolution(data.chain);
+      setLoading(false);
+    });
+  }, [pokemon]);
+
+  useEffect(() => {
+    getNextEvolution();
+
+    // eslint-disable-next-line
+  }, [currentEvolution]);
+
+  const getNextEvolution = () => {
+    if (
+      currentEvolution.length === 0 ||
+      currentEvolution.evolves_to.length === 0
+    )
+      return null;
+
+    const nextEvolution = currentEvolution.evolves_to[0];
+    const details = nextEvolution.evolution_details[0];
+
+    // extract useful data
+    const current = currentEvolution.species.name;
+    const next = nextEvolution.species.name;
+    const trigger = triggersDisplayName[details.trigger.name];
+    const triggerValue =
+      details.min_level ||
+      details.min_happiness ||
+      details.item?.name.replace("-", " ") ||
+      "";
+    const currentId = extractId(currentEvolution.species.url);
+    const nextId = extractId(nextEvolution.species.url);
+
+    // get images URL
+    const currentImage = getImageURL(currentId);
+    const nextImage = getImageURL(nextId);
+
+    // set current evolution to next evolution
+    setCurrentEvolution((prev) => prev.evolves_to[0]);
+
+    // push new evolution object
+    setEvolutionChain((prev) => {
+      return [
+        ...prev,
+        {
+          current,
+          next,
+          trigger,
+          triggerValue,
+          currentId,
+          nextId,
+          currentImage,
+          nextImage,
+        },
+      ];
+    });
+  };
+
+  const getImageURL = (id) => {
+
+    const baseURL =
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other";
 
 
-    const [ currentEvolution, setCurrentEvolution ] = useState( [] );
-    const [ evolutionChain, setEvolutionChain ] = useState( [] );
-    const [ loading, setLoading ] = useState( true );
-    
-    const { t } = useTranslation();
-    const triggersDisplayName = {
+    return `${baseURL}/official-artwork/${id}.png`;
+  };
 
-        'level-up': 'Lvl',
-        'trade': 'Trade',
-        'use-item': 'Use'
-
-    };
-
-
-    useEffect( () => {
-
-        setLoading( true );
-
-        fetchPokemonEvolutionChain( pokemon.id ).then( ( data ) => {
-
-            setEvolutionChain( [] );
-            setCurrentEvolution( data.chain );
-            setLoading( false );
-
-        });
-
-    }, [ pokemon ]);
-
-
-    useEffect( () => {
-
-        getNextEvolution();
-
-        // eslint-disable-next-line
-    }, [ currentEvolution ])
-
-
-    const getNextEvolution = () => {
-
-        if( currentEvolution.length === 0 || currentEvolution.evolves_to.length === 0 )
-            return null;
-
-
-        const nextEvolution = currentEvolution.evolves_to[0];
-        const details       = nextEvolution.evolution_details[0];
-        
-
-        // extract useful data
-        const current       = currentEvolution.species.name;
-        const next          = nextEvolution.species.name;
-        const trigger       = triggersDisplayName[details.trigger.name];
-        const triggerValue  = details.min_level || details.min_happiness || details.item?.name.replace( '-', ' ' ) || '';
-        const currentId     = extractId( currentEvolution.species.url );
-        const nextId        = extractId( nextEvolution.species.url );
-
-        // get images URL
-        const currentImage  = getImageURL( currentId );
-        const nextImage     = getImageURL( nextId );
-
-
-        // set current evolution to next evolution
-        setCurrentEvolution( prev => ( prev.evolves_to[0] ));
-
-        // push new evolution object
-        setEvolutionChain( prev => {
-
-            return [ ...prev, { 
-                current,
-                next,
-                trigger,
-                triggerValue,
-                currentId,
-                nextId,
-                currentImage,
-                nextImage
-            }];
-            
-        });
-
+  const fetchPokemon = (name) => {
+    if (!name || pokemon.name === name) {
+      return;
     }
 
-    return (
-        <div>
-            <h1>evolution</h1>
-        </div>
-    )
+    setLoading(true);
+    fetchPokemonData(name).then((data) => {
+      changePokemon(data);
+    });
+  };
+
+  return (
+    <div>
+      <h1>evolution</h1>
+    </div>
+  );
+};
+
+const extractId = ( url ) => {
+
+    return url.match( /\/(\d+)\// )[1];
+
 }
